de5a6e3d35423c1f1935077e1dafab3dede299cf
shxnpie <shxnpie@gmail.com>
2020-04-11 22:02:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Covid India 
+## Info:
+### A pluggable command/module for [Kandnub's][kan] [TG-UserBot][userbot].
+This command uses the [covid-india][covid] pip package.
+#
+## Usage:
+Do ```.covin "State/State's Abbrevation"```
+#
+## Installation:
+Installation is pretty simple, just copy this repo's link and paste it in the [repo section][repo] of the config and 
+retsart the script!
+
+[//]: # (Comment)
+  [kan]: <https://www.github.com/kandnub>
+  [userbot]: <https://github.com/kandnub/TG-UserBot>
+  [covid]: <https://pypi.org/project/covid-india/>
+  [repo]: <https://github.com/kandnub/TG-UserBot/blob/19f4d2eadd3f81222740f6663133a58d12be7928/sample_config.ini#L36>
---
+++ covin.py
@@ -0,0 +1,61 @@
+import covid_india
+
+from covid_india import states
+from userbot import client
+from userbot.utils.events import NewMessage
+
+plugin_category = "pandemic"
+
+cov_str = f"""`{'Confirmed':<9}:` **%(Total)s**
+`{'Recovered':<9}:`  **%(Cured)s**
+`{'Deaths':<9}:`  **%(Death)s**"""
+
+cdict = {
+    'AN':'Andaman and Nicobar Islands',
+    'AP':'Andhra Pradesh', 'AR':'Arunachal Pradesh',
+    'AS':'Assam','BR':'Bihar','CH':'Chandigarh',
+    'CG':'Chhattisgarh','DL':'Delhi','GA':'Goa',
+    'GJ':'Gujarat','HR':'Haryana','HP':'Himachal Pradesh',
+    'JK':'Jammu and Kashmir','JH':'Jharkhand','KA':'Karnataka',
+    'KL':'Kerala','MP':'Madhya Pradesh','MH':'Maharashtra',
+    'MN':'Manipur','ML':'Mizoram','OR':'Odisha','PY':'Puducherry',
+    'PB':'Punjab','RJ':'Rajasthan','TN':'Tamil Nadu','TG':'Telengana',
+    'TR':'Tripura','UP':'Uttar Pradesh','UK':'Uttarakhand','WB':'West Bengal'
+}
+
+@client.onMessage(
+    command="corona",
+    outgoing=True, regex="(?:corona_in|covin)(?: |$)(.*)"
+)
+async def ncov(event: NewMessage.Event) -> None:
+    """Testing"""
+    covid = covid_india
+    catch = event.matches[0]
+    match = event.matches[0].group(1)
+    if match:
+        strings = []
+        failed = []
+        args, c = await client.parse_arguments(match)
+        
+        if match.lower() == "states":
+            strings = sorted(covid.states.getdata())
+        else:
+            for c in args:
+                try:
+                    await event.answer("`Parsing data...`")
+                    state = (cdict[c.upper()] if c.upper() in cdict else c.title())
+                    state_data = covid.states.getdata(state)
+                    string = f"COVID-19 stats for:  `{state}`\n"
+                    string += cov_str % state_data
+                    strings.append(string)
+                except KeyError:
+                    failed.append(state)
+                    continue
+        if strings:
+            await event.answer('\n\n'.join(strings))
+        if failed:
+            await event.answer(f"Couldn't find stats for: `{state}`.\nDouble check for spelling errors.")
+    if not match:
+        await event.edit("**Usage**: `.covin 'State/Abbrevation'`.")
+        return
+        
